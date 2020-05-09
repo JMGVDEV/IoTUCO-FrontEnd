36bcaeabede5559fe26985131bfe49c492b7158d
SantiSanH <santisanchez10@hotmail.es>
2020-05-09 14:44:41 -0500

    Agrego componente de enfermedades en Home

---
+++ src/components/ShowingDisease.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+
+export default class ShowD extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      diseases: ['Roya', 'Manchas'],
+      Observation: 'tiene mucha enfermedad, fumigar',
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="text-center">
+          <ListGroup>
+            <ListGroup.Item variant="danger">
+              Enfermedades de la Planta:
+            </ListGroup.Item>
+            {this.state.diseases.map((disease, id) => (
+              <ListGroup.Item key={id}>{disease}</ListGroup.Item>
+            ))}
+          </ListGroup>
+        </div>
+        <Col className="col-md-8">
+          <div className="text-center">
+            <ListGroup>
+              <ListGroup.Item variant="warning">Observaciones:</ListGroup.Item>
+              <ListGroup.Item>{this.state.Observation}</ListGroup.Item>
+            </ListGroup>
+          </div>
+        </Col>
+      </React.Fragment>
+    );
+  }
+}
