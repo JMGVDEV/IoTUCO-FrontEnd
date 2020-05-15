cb5098f82e05493ba24b58ced65b7cea71754e9e
Juan Diego Cardona <jd99@outlook.com>
2020-05-15 16:45:44 -0500

    Build

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -2,8 +2,8 @@
  * URL SERVER
  */
 
-//const SERVER_URL = 'http://3.22.57.173:3000';
-const SERVER_URL = 'http://127.0.0.1:3001';
+const SERVER_URL = 'http://34.238.41.157:3001';
+//const SERVER_URL = 'http://127.0.0.1:3001';
 
 /*
  * SERVER PATHS
